1b3cd8439c918b9710dcb9c995b1184371237929
yaric111 <yaroslav.vasilenko3101@gmail.com>
2023-09-21 20:04:09 +0300

    fix: cors

--- internal/router/router.go
+++ internal/router/router.go
@@ -12,7 +12,16 @@ import (
 func NewApiRouter(controllers *modules.Controllers, components *component.Components) *chi.Mux {
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
-	r.Use(cors.AllowAll().Handler)
+	r.Use(cors.Handler(cors.Options{
+		// AllowedOrigins:   []string{"https://foo.com"}, // Use this to allow specific origin hosts
+		AllowedOrigins: []string{"https://*", "http://*"},
+		// AllowOriginFunc:  func(r *http.Request, origin string) bool { return true },
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+	}))
+
 	r.Use(middleware.Recoverer)
 
 	r.Use(middleware.Timeout(60 * time.Second))
