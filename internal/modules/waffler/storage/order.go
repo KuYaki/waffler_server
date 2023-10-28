55d55a92385ef592d4f8c505bcbd68f4d5e2eb68
eisandbar <eisandbar@gmail.com>
2023-10-28 03:01:38 -0400

    feat(waffler): Add special sorting of nulls
    For the main order parameter null values are listed last.
    For suborder parameters nulls are treated as the largest value.
    
    Updated test db range of fake values

--- internal/modules/waffler/storage/order.go
+++ internal/modules/waffler/storage/order.go
@@ -4,7 +4,10 @@ import "gorm.io/gorm"
 
 func ApplyOrder(conn *gorm.DB, orders []string) (tx *gorm.DB) {
 	tx = conn
-	for _, order := range orders {
+	for i, order := range orders {
+		if i == 0 {
+			order += " NULLS LAST"
+		}
 		tx = tx.Order(order)
 	}
 	return
